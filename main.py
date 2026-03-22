701a0871f45da3e9e9a3526cb7846b13ca7f69f3
wedersonCD <wedersoncabral@gmail.com>
2026-03-22 09:09:57 -0300

    Add activity description

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import pandas
 
 dataFrame_lifeExpectancy_raw = pandas.read_csv('life_expectancy.csv', delimiter=';', decimal=',')
 
-dataFrame_lifeExpectancy = dataFrame_lifeExpectancy_raw[["Country","Region","Year","Economy_status_Developed","Under_five_deaths"]]
+dataFrame_lifeExpectancy = dataFrame_lifeExpectancy_raw[["Country","Region","Year","Economy_status_Developed","Population_mln","Under_five_deaths"]]
 
 print(dataFrame_lifeExpectancy.head(5))
