d7b7ec41b084a4dcccf945aec1a88d02ad8c3fc6
wedersonCD <wedersoncabral@gmail.com>
2026-03-22 13:41:01 -0300

    Inclusão mais estatisticas e apresentação da média e mediana no histograma

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
 import pandas
+import matplotlib.pyplot as pyplot
 
 dataFrame_lifeExpectancy_raw = pandas.read_csv('life_expectancy.csv', delimiter=';', decimal=',')
+dataFrame_lifeExpectancy = dataFrame_lifeExpectancy_raw[["Country","Region","Year","Economy_status_Developed","Under_five_deaths"]]
 
-dataFrame_lifeExpectancy = dataFrame_lifeExpectancy_raw[["Country","Region","Year","Economy_status_Developed","Population_mln","Under_five_deaths"]]
+dataFrame_underFiveDeaths = dataFrame_lifeExpectancy["Under_five_deaths"]
+#Imprime a tabela estatistica da variavel
+print(dataFrame_underFiveDeaths.describe())
+print('Amplitude:', dataFrame_underFiveDeaths.max() - dataFrame_underFiveDeaths.min())
+print('Distancia Interquartilica:', dataFrame_underFiveDeaths.quantile(0.75) - dataFrame_underFiveDeaths.quantile(0.25))
+print('Coeficiente de variação:', dataFrame_underFiveDeaths.std() / dataFrame_underFiveDeaths.mean())
+print('Mode:', dataFrame_underFiveDeaths.mode())
+print('Assimetria:', dataFrame_underFiveDeaths.skew())
+print('Curtose:', dataFrame_underFiveDeaths.kurtosis())
 
-print(dataFrame_lifeExpectancy.head(5))
+
+
+
+figures, axis = pyplot.subplots(1,2)
+
+axis[0].hist(dataFrame_underFiveDeaths, bins=30, edgecolor='black')
+axis[0].set_title('Histograma de Mortes antes dos 5 anos por 1.000 pessoas')
+axis[0].set_xlabel('Morte antes dos 5 anos por 1.000 pessoas')
+axis[0].set_ylabel('Frequência')
+
+axis[0].axvline(dataFrame_underFiveDeaths.mean(),color="red",label="Média")
+axis[0].axvline(dataFrame_underFiveDeaths.median(),color="blue",label="Mediana")
+axis[0].legend(loc="upper right")
+
+axis[1].boxplot(dataFrame_underFiveDeaths)
+axis[1].set_title('Boxplot de Mortes antes dos 5 anos por 1.000 pessoas')
+
+pyplot.show()
+
+
